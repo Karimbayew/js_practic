85a5f0bd4ac3aa80151003079a33c7dced38c1f3
Shazhad <karimbayewshahzad@gmail.com>
2022-12-09 00:54:14 +0500

    add and if and for

--- js/scripr.js
+++ js/scripr.js
@@ -10,16 +10,31 @@ const personalMovieDB = {
     privat: false
 };
 
-numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 personalMovieDB.count = numberOfFilms;
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count <= 10) {
+    console.log('Просмотрено давольно мало фильм');
+} else if (personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
 
-// console.log(numberOfFilms);
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+    
 console.log(personalMovieDB);
